7b9b84434413fa5e575afe5e45c64a05bebf5948
Michael Bisgaard Olesen <mol@in-commodities.com>
2026-01-19 10:16:41 +0100

    Implement

---
+++ README.md
@@ -0,0 +1,24 @@
+# PrimeVue InputNumber debug app
+
+[Vite](https://vite.dev/)/[Vue](https://vuejs.org/) app for understanding the order and contents of events emitted by
+[PrimeVue's](https://primevue.org/) [`InputNumber`](https://primevue.org/inputnumber) component.
+
+## Install
+
+```shell
+npm install
+```
+
+## Run
+
+```shell
+npm run dev
+```
+
+## Use
+
+* Locate the input field
+* Change its contents
+* Observe what values are emitted by viewing the page and console log.
+* Figure out what the hell the purpose of `formattedValue` is (and please let me know if you do!).
+* Delete this project and forget that it ever existed.
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import InputNumber from "primevue/inputnumber"
+import {ref} from "vue"
+
+const input = ref(0)
+const emittedByAtInputValue = ref()
+const emittedByAtInputFormattedValue = ref()
+const emittedByAtUpdateModelValue = ref()
+
+function onAtInput({value, formattedValue}) {
+  console.log('onAtInput', {value, formattedValue, inputValue: input.value})
+  emittedByAtInputValue.value = value
+  emittedByAtInputFormattedValue.value = formattedValue
+}
+
+function onAtUpdateModelValue(value) {
+  console.log('onAtUpdateModelValue', {value, inputValue: input.value})
+  emittedByAtUpdateModelValue.value = value
+}
+</script>
+
+<template>
+  Input: <InputNumber v-model="input" style="width:100%" placeholder="here" @input="onAtInput" @update:modelValue="onAtUpdateModelValue"></InputNumber>
+  <ul>
+    <li>Number emitted by <code>@input</code> (.value): <code>{{emittedByAtInputValue}}</code></li>
+    <li>Number emitted by <code>@input</code> (.formattedValue): <code>{{emittedByAtInputFormattedValue}}</code></li>
+    <li>Number emitted by <code>@update:modelValue</code>: <code>{{emittedByAtUpdateModelValue}}</code></li>
+  </ul>
+
+</template>
